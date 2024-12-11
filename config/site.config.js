e2c548336856af893fcf810bfa825a7ebc88b6b3
苍术 <129147116+NiuHK@users.noreply.github.com>
2024-12-11 15:21:44 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -11,7 +11,7 @@ module.exports = {
 
   // [OPTIONAL] This is the website icon to the left of the title inside the navigation bar. It should be placed under the
   // /public directory of your GitHub project (not your OneDrive folder!), and referenced here by its relative path to /public.
-  icon: '/icons/ico.svg',
+  icon: '/icons/128.svg',
 
   // The name of your website. Present alongside your icon.
   title: "Daniel's workspace",
@@ -35,7 +35,7 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-    'Powered by <a href="https://github.com/lyc8503/onedrive-cf-index-ng" target="_blank" rel="noopener noreferrer">onedrive-cf-index-ng</a>. Made with ❤ by lyc8503.',
+    ' Made with ❤ .',
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
